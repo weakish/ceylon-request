fb95a56ba575ad9f1069f224007dea6a7c4f5bb6
Jakukyo Friel <weakish@gmail.com>
2016-10-23 22:26:19 +0800

    Add api doc.

---
+++ api/resources/index.js
@@ -0,0 +1,12 @@
+var index = [
+{'name': 'io.github.weakish.request', 'type': 'package', 'url': 'index.html#section-package', 'doc': '', 'tags': [],'icons': ['icon-package']},
+{'name': 'RequestException', 'type': 'class', 'url': 'RequestException.type.html', 'doc': '<p>Root exception.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'ConnectionError', 'type': 'class', 'url': 'ConnectionError.type.html', 'doc': '<p>Network problems such as DNS failure and not connected.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'HttpError', 'type': 'class', 'url': 'HttpError.type.html', 'doc': '<p>Thrown if the HTTP request returned an unsuccessful status code.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'TimeoutError', 'type': 'class', 'url': 'TimeoutError.type.html', 'doc': '<p>Thrown if a request times out.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'TooManyRedirectsError', 'type': 'class', 'url': 'TooManyRedirectsError.type.html', 'doc': '<p>Thrown if specified number of maximum redirections exceeded.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'get', 'type': 'function', 'url': 'index.html#get', 'doc': '<p>Given a Url String, returns GET Response content when success (200),\notherwise returns status code.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'try_get', 'type': 'function', 'url': 'index.html#try_get', 'doc': '<p>Like <a class=\'link\' href=\'index.html#get\' title=\'Go to io.github.weakish.request::get\'><code><span class=\'identifier\'>get()</span></code></a>, but throws customizable exception on 404, and HttpError for others.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'io.github.weakish.request', 'type': 'module', 'url': 'index.html', 'doc': '<p>This module provides http client functions on JVM and JS.</p>', 'tags': [],'icons': ['icon-module']}];
+var tagIndex = [
+];
